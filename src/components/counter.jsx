74dbdbc56e8de58961f04d5bb287f9c9be2c319e
dmuhin <dmitry.muhin@zfx.com>
2021-09-07 14:18:22 +0500

    lesson 3

---
+++ src/components/counter.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Counter = (props) => {
+  const formValue = () => {
+    return props.value === 0 ? "Ноль" : props.value;
+  };
+  const getBadgeClasses = () => {
+    let classes = "badge m-2 bg-";
+    classes += props.value === 0 ? "danger" : "primary";
+    return classes;
+  };
+  return (
+    <div>
+      <h4>{props.name}</h4>
+      <span className={getBadgeClasses()}>{formValue()}</span>
+      <button
+        className="btn btn-secondary btn-sm m-2"
+        onClick={() => props.onIncrement(props.id)}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => props.onDecrement(props.id)}
+        className="btn btn-secondary btn-sm  m-2"
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => props.onDelete(props.id)}
+        className="btn btn-danger btn-sm m-2"
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
